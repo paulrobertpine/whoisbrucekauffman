471466990d9a444b92c09971b44e3dc2c6a46cc3
Paul Clifford <clifford@queensu.ca>
2018-09-25 14:59:05 -0400

    BG image, remove files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Who is Bruce Kauffman?',
+    author: 'Paul Clifford',
+    description: 'website for film Who is Bruce Kauffman?',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -15,7 +15,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/WIBK_Question_Mark_White.svg', 
       },
     },
     'gatsby-plugin-sass',

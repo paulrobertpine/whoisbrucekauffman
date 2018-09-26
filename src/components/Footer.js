a1af62a13c40a37a6ad151a6fab17570ec56a558
Paul Clifford <clifford@queensu.ca>
2018-09-26 16:31:51 -0400

    social icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,42 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 const Footer = props => (
-  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-    <p className="copyright">
-      social icons go here
-    </p>
+  <footer id="footer" style={props.timeout ? { display: "none" } : {}}>
+    <ul className="icons">
+      <li>
+        <a
+          href="https://twitter.com/whoisbrucefilm"
+          className="icon fa-twitter"
+          target="_blank"
+        >
+          <span className="label">Twitter</span>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.facebook.com/whoisbrucekauffmanfilm/"
+          className="icon fa-facebook"
+          target="_blank"
+        >
+          <span className="label">Facebook</span>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.instagram.com/whoisbrucekauffman/"
+          className="icon fa-instagram"
+          target="_blank"
+        >
+          <span className="label">Instagram</span>
+        </a>
+      </li>
+    </ul>
   </footer>
-)
+);
 
 Footer.propTypes = {
-  timeout: PropTypes.bool,
-}
+  timeout: PropTypes.bool
+};
 
-export default Footer
+export default Footer;

91c5810e2edbdcd6a085504fec9658935f31a725
Paul Clifford <clifford@queensu.ca>
2018-10-02 16:27:17 -0400

    updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,6 +11,7 @@ import Still from "./Still";
 // images
 import wibkBlack from "../images/WIBK_Full_White.svg";
 import bruce from "../images/stills/bruce.png";
+import bruceWaterfront from "../images/stills/bruce-waterfront-still.jpg";
 import heroux from "../images/stills/heroux.jpg";
 import krolik from "../images/stills/krolik.png";
 import mackey from "../images/stills/mackey.png";
@@ -19,6 +20,8 @@ import teaser from "../images/stills/teaser.png";
 import KCCF from "../images/partners/KCFF_logo-white.png";
 import KWF from "../images/partners/KWF.png";
 import SPAF from "../images/partners/SPAF_logo.png";
+import City from "../images/partners/city-of-kingston-logo.jpg";
+import KAC from "../images/partners/KAC-Identity-Centered-BW.jpg";
 
 const YouTubeOpts = {
   height: "370px",
@@ -32,15 +35,14 @@ const YouTubeOpts = {
 class Main extends React.Component {
   render() {
     const settings = {
-      arrows: false,
+      arrows: true,
       autoplay: true,
-      pauseOnHover: false,
-      speed: 1000,
-      className: "center",
       centerMode: true,
-      vertical: true,
+      className: "center",
       infinite: true,
-      slidesToShow: 2
+      pauseOnHover: true,
+      slidesToShow: 1,
+      speed: 2000
     };
 
     let close = (
@@ -70,46 +72,71 @@ class Main extends React.Component {
             <img src={wibkBlack} alt="Who Is Bruce Kaufmann logo" />
           </span>
           <p>
-            Who Is Bruce Kauman? is an anthology film that explores and
+            Who Is Bruce Kauffman? is an anthology film that explores and
             celebrates Kingston, Ontario’s vibrant poetic community through the
             eyes of a poet impresario who manages to be both ubiquitous and
-            elusive. Kauman’s influence is broad and longstanding – if one
+            elusive. Kauffman’s influence is broad and longstanding – if one
             proceeds from any given place on an imagined map of Kingston’s
             poetry scene, then Bruce will show up somewhere along the trail,
-            humbly and unassumingly encouraging all local writers. In this film,
-            the city’s most accomplished film makers and other artists
-            collaborate with some of Kingston’s finest poets to interpret their
-            poems in short cinematic pieces. They join Kauman in giving us a
-            snapshot of the city and its poetry.
+            humbly and unassumingly encouraging all local writers.
+          </p>
+
+          <p>
+            This film follows a day in the life of Bruce Kauffman, from his
+            long-standing radio show on CFRC 101.9 FM, the Queen’s Campus
+            Community Radio Station, to his “…and the journey continues” open
+            mic, the longest running open mic series in Kingston, to his work as
+            editor and mentor to countless young poets, to his passionate
+            involvement in poetry readings, workshops, published anthologies,
+            book launches, and arts festivals all over the beautiful limestone
+            city. It is a love letter to Bruce, the living legend of Canada’s
+            most literary town, who has given so much of himself to his
+            community.
+          </p>
+
+          <p>
+            In the film, the city’s most accomplished film makers collaborate
+            with some of Kingston’s finest poets to interpret their poems in
+            short cinematic vignettes, calling on the artistry of the local
+            community to bring their vision to life. Stilt-walkers, swimmers,
+            dancers, musicians and actors all come together to offer audiences a
+            unique doorway into the vibrant, underground world that is
+            Kingston’s literary scene.
+          </p>
+
+          <p>
+            They join Kauffman in giving us a snapshot of the city and its
+            poetry.
           </p>
 
           <h3>FEATURING KINGSTON POETS</h3>
 
           <ul>
-            <li>Steven Heighton</li>
-            <li>Sarah Tsiang</li>
-            <li>Fernando Monte</li>
-            <li>Sadiqa de Meijer</li>
             <li>Alyssa Cooper</li>
-            <li>Armand Ruo</li>
+            <li>Armand Ruffo</li>
+            <li>Fernando Monte</li>
             <li>Helen Humphreys</li>
             <li>Jason Heroux </li>
+            <li>Sadiqa de Meijer</li>
+            <li>Sarah Tsiang</li>
+            <li>Steven Heighton</li>
           </ul>
 
           <h3>FEATURING KINGSTON FILM MAKERS </h3>
 
           <ul>
-            <li>Elisa Paloschi</li>
-            <li>Nuno Ramalho</li>
-            <li>Julia Krolik</li>
-            <li>Owen Fernley</li>
             <li>Clarke Mackey</li>
-            <li>Armand Ruo</li>
-            <li>Don Maynard</li>
             <li>Craig Berggold</li>
-            <li>Max Montalvo</li>
-            <li>Josh Lyon</li>
+            <li>Don Maynard</li>
+            <li>Elisa Paloschi</li>
             <li>James Greatrex</li>
+            <li>Josh Lyon</li>
+            <li>Julia Krolik</li>
+            <li>Leigh Ann Bellamy</li>
+            <li>Max Montalvo</li>
+            <li>Nuno Ramalho</li>
+            <li>Owen Fernley</li>
+            <li>Wanda Praamsma</li>
           </ul>
         </article>
 
@@ -137,7 +164,8 @@ class Main extends React.Component {
           <h2 className="major">Stills</h2>
           <p>click to open/download</p>
           <Slider {...settings}>
-            <Still image={bruce} alt="bruce"/>
+            <Still image={bruce} alt="bruce" />
+            <Still image={bruceWaterfront} alt="bruceWaterfront" />
             <Still image={heroux} alt="heroux" />
             <Still image={krolik} alt="krolik" />
             <Still image={teaser} alt="teaser" />
@@ -159,6 +187,8 @@ class Main extends React.Component {
           <Still image={KCCF} alt="KCCF" />
           <Still image={KWF} alt="KWF" />
           <Still image={SPAF} alt="SPAF" />
+          <Still image={City} alt="City" />
+          <Still image={KAC} alt="KAC" />
         </article>
 
         <article
